264644fe61ac88ad33f9ed803dc1217f7adf59bb
VlasovVasylii <140720296+VlasovVasylii@users.noreply.github.com>
2025-05-03 02:09:33 +0600

    lr7

---
+++ ООП/lr7/src/main/java/lr7/FeedbackServlet.java
@@ -0,0 +1,31 @@
+package lr7;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/feedback")
+public class FeedbackServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/feedback.html").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String message = req.getParameter("message");
+
+        System.out.println("Имя: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Сообщение: " + message);
+
+        resp.sendRedirect("/home");
+    }
+}
